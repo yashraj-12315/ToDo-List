eb9d2a58f8ada2c8ac1506f4ad153cbcf695b3a7
yashraj-12315 <yashraj4586@gmail.com>
2024-03-13 15:29:26 +0530

    pushing

---
+++ server/database/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+const Connection = () => {
+  const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.w4ffpfu.mongodb.net/`;
+  mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+  mongoose.connection.on("connected", () => {
+    console.log("Database connected Successfully");
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Database disconnected");
+  });
+
+  mongoose.connection.on("error", (error) => {
+    console.log("Error while connecting with the database ", error.message);
+  });
+};
+
+export default Connection;
